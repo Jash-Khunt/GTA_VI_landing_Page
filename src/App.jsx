a05bf32b6cf1fbf8a9de5bec47eb5021a2b980b9
Apple <apple@192.168.1.4>
2025-09-27 21:31:10 +0530

    Completed Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import gsap from "gsap"
 import {ScrollTrigger} from "gsap/all"
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-pink text-3xl">Welcome to the world of gta and gsap!</h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   )
 }
---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav>
+        <img className="scale-90" src="/images/nav-logo.svg" alt="nav-logo" />
+        <img className="w-10" src="/images/menu.svg" alt="menu" />
+      </nav>
+  )
+}
+
+export default NavBar   
